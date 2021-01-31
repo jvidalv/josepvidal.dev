2fac30a507fcc2def998839b45fc1a05245c884b
Josep Vidal <josepvidalvidal@gmail.com>
2021-01-31 09:45:42 +0100

    ⛵ Improved loading animation

--- src/components/containers/about-me/about-me.tsx
+++ src/components/containers/about-me/about-me.tsx
@@ -1,14 +1,28 @@
 import { FC } from 'preact/compat';
 import { ComponentChildren, h } from 'preact';
+import { useIntersectionObserver } from 'hooks/use-intersection-observer';
+import classNames from 'classnames';
 
+const animations = require('styles/animations.pcss');
 const styles = require('./about-me.pcss');
 
 type Props = {
   children: ComponentChildren;
 };
 
-export const AboutMe: FC<Props> = ({ children }) => (
-  <div className={styles.aboutMe}>
-    <div className={styles.aboutMe__inner}>{children}</div>
-  </div>
-);
+export const AboutMe: FC<Props> = ({ children }) => {
+  const [setNode, isIntersecting, hasIntersected] = useIntersectionObserver<HTMLElement>();
+  const className = classNames({
+    [styles.aboutMe__inner]: true,
+    [animations.scale__base]: true,
+    [animations.scale__entered]: isIntersecting || hasIntersected,
+  });
+
+  return (
+    <div className={styles.aboutMe}>
+      <div ref={setNode} className={className}>
+        {children}
+      </div>
+    </div>
+  );
+};
--- src/components/containers/row/row.tsx
+++ src/components/containers/row/row.tsx
@@ -1,26 +1,14 @@
 import { FC } from 'preact/compat';
 import { ComponentChildren, h } from 'preact';
-import classNames from 'classnames';
-import { useIntersectionObserver } from 'hooks/use-intersection-observer';
 
-const animations = require('styles/animations.pcss');
 const styles = require('./row.pcss');
 
 type Props = {
   children: ComponentChildren;
 };
 
-export const Row: FC<Props> = ({ children }) => {
-  const [setNode, isIntersecting, hasIntersected] = useIntersectionObserver<HTMLElement>();
-  const className = classNames({
-    [styles.row]: true,
-    [animations.scale__base]: true,
-    [animations.scale__entered]: isIntersecting || hasIntersected,
-  });
-
-  return (
-    <section ref={setNode}>
-      <div className={className}>{children}</div>
-    </section>
-  );
-};
+export const Row: FC<Props> = ({ children }) => (
+  <section>
+    <div className={styles.row}>{children}</div>
+  </section>
+);
