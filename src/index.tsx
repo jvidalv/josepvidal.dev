c328fc142efc3a6cf9b7b56f2c97806c679953fa
Josep Vidal <josepvidalvidal@gmail.com>
2021-07-10 13:10:01 +0200

    feat: added google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import 'preact/debug';
 import { h, render } from 'preact';
 import { App } from './core/app';
 import './index.css';
+import ReactGA from 'react-ga';
 
 if (process.env.NODE_ENV === 'development') {
   require('preact/debug');
 }
+
+ReactGA.initialize('UA-155356290-3', {
+  testMode: process.env.NODE_ENV === 'test',
+  debug: process.env.NODE_ENV === 'development',
+});
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 render(<App />, document.getElementById('app'));
