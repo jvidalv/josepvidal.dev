2e2acec58e09235a34e44f5a5ac31f1d7160b88c
jvidalv <josepvidalvidal@gmail.com>
2023-03-01 18:57:27 +0100

    feat: configured tailwind and added fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const basier = localFont({
+  src: [
+    {
+      path: "../../fonts/basiercircle-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../fonts/basiercircle-bold.otf",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+  variable: "--basier-font",
+  weight: "400",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={basier.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
