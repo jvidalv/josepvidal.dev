5142c8003b5f49484fb1a03e19efeb818ee52da1
jvidalv <josepvidalvidal@gmail.com>
2024-12-10 14:24:19 +0100

    feat: improve mobile ux

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
     },
   ];
   return (
-    <footer className="flex justify-between mt-20 border-t-4 border-dashed pt-6 font-mono">
+    <footer className="flex flex-col gap-4 sm:flex-row justify-between mt-20 border-t-4 border-dashed pt-6 font-mono">
       <h3>jvidal.dev ✨ {new Date().getFullYear()}</h3>
       <div className="flex gap-6">
         {links.map(({ href, name }) => (
